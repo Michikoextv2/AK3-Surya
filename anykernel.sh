bc30dafda7fdcd9bee2282873da9cbce37c65ec5
Whoami? <bagasabiyoso10@gmail.com>
2026-08-14 18:32:16 +0700

    Update kernel string in AnyKernel setup

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=OSS Kernel | POCO X3 NFC
+kernel.string=OSS Kernel | Based Rethinking
 do.devicecheck=1
 do.modules=0
 do.systemless=1
